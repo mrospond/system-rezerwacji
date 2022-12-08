c35c98ddd8fbcf687ab04eae64c776d4e8067245
rfmajor <rfmajor99@gmail.com>
2022-12-08 11:03:33 +0100

    Add prints

--- db-init/db_init.py
+++ db-init/db_init.py
@@ -4,7 +4,6 @@ import csv
 DB_NAME = 'reservation_system'
 DB_USER = 'admin'
 DB_PASSWORD = 'admin'
-DB_URL = 'jdbc:postgresql://localhost:5432/reservation_system'
 DB_HOST = 'localhost'
 
 INIT_FILE = 'init.sql'
@@ -15,14 +14,17 @@ TABLE_NAMES = ['regions', 'countries', 'cities', 'buildings', 'rooms', 'employee
 def init_database():
     connection = None
     try:
+        print('Connecting to database')
         connection = psycopg2.connect(
             host=DB_HOST,
             database=DB_NAME,
             user=DB_USER,
             password=DB_PASSWORD)
         cursor = connection.cursor()
+        print('Creating tables')
         cursor.execute(load_sql_init())
         for table in TABLE_NAMES:
+            print(f'Updating table: {table}')
             load_csv_to_db(table, cursor)
             connection.commit()
         cursor.close()
@@ -31,7 +33,7 @@ def init_database():
     finally:
         if connection is not None:
             connection.close()
-            print('Database connection closed.')
+            print('Database connection closed')
 
 
 def load_sql_init():
@@ -56,7 +58,7 @@ def load_csv_to_db(table_name, cursor):
                 header_skipped = True
         statement = "INSERT INTO " + table_name + " VALUES(" + (len(rows[0]) - 1) * "%s," + "%s);"
         duplicates, distinct_rows = check_for_duplicates(rows)
-        print(f'Found {duplicates} duplicate values for table {table_name}')
+        print(f'Dropped {duplicates} duplicate values for table: {table_name}')
         cursor.executemany(statement, distinct_rows)
 
 
