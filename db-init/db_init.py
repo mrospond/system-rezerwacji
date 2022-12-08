0a2df1beb1d2f6942d3e75a560036b2edc19ce33
rfmajor <rfmajor99@gmail.com>
2022-12-08 01:23:05 +0100

    Add sql initializer files

---
+++ db-init/db_init.py
@@ -0,0 +1,82 @@
+import psycopg2
+import csv
+
+DB_NAME = 'reservation_system'
+DB_USER = 'admin'
+DB_PASSWORD = 'admin'
+DB_URL = 'jdbc:postgresql://localhost:5432/reservation_system'
+DB_HOST = 'localhost'
+
+INIT_FILE = 'init.sql'
+CSV_DIR = '../csv-init/output/'
+TABLE_NAMES = ['regions', 'countries', 'cities', 'buildings', 'rooms', 'employees']
+
+
+def init_database():
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD)
+        cursor = connection.cursor()
+        cursor.execute(load_sql_init())
+        for table in TABLE_NAMES:
+            load_csv_to_db(table, cursor)
+            connection.commit()
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
+
+
+def load_sql_init():
+    script = None
+    with open(INIT_FILE, "r") as file:
+        script = file.read()
+    return script
+
+
+def load_csv_to_db(table_name, cursor):
+    rows = []
+    with open(CSV_DIR + table_name + '.csv') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        header_skipped = False
+        for row in reader:
+            for i in range(len(row)):
+                if row[i] == '':
+                    row[i] = None
+            if header_skipped:
+                rows.append(row)
+            else:
+                header_skipped = True
+        statement = "INSERT INTO " + table_name + " VALUES(" + (len(rows[0]) - 1) * "%s," + "%s);"
+        duplicates, distinct_rows = check_for_duplicates(rows)
+        print(f'Found {duplicates} duplicate values for table {table_name}')
+        cursor.executemany(statement, distinct_rows)
+        cursor.execute("SELECT * FROM " + table_name + ";")
+
+
+def check_for_duplicates(rows):
+    distinct = set()
+    new_rows = []
+    duplicates = 0
+    for row in rows:
+        if not distinct.__contains__(row[0]):
+            distinct.add(row[0])
+            new_rows.append(row)
+        else:
+            duplicates += 1
+    return duplicates, new_rows
+
+
+def main():
+    init_database()
+
+
+if __name__ == '__main__':
+    main()
---
+++ db-init/init.sql
@@ -0,0 +1,81 @@
+DROP TABLE IF EXISTS reservations;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS buildings;
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS regions;
+
+CREATE TABLE regions(
+    id INT,
+    name varchar(50),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE countries(
+    id INT,
+    country_code VARCHAR(5),
+    name VARCHAR(30),
+    region_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(region_id) REFERENCES regions(id)
+);
+
+CREATE TABLE cities(
+    id INT,
+    name VARCHAR(50),
+    country_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(country_id) REFERENCES countries(id)
+);
+
+CREATE TABLE buildings(
+    id INT,
+    name VARCHAR(10),
+    floors INT,
+    city_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(city_id) REFERENCES cities(id)
+);
+
+CREATE TABLE rooms(
+    id INT,
+    building_id INT,
+    priority INT,
+    status VARCHAR(30),
+    floor INT,
+    name VARCHAR(50),
+    seats INT,
+    video_conference_holder BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY(building_id) REFERENCES buildings(id)
+);
+
+CREATE TABLE employees(
+    id VARCHAR(10),
+    city_id INT,
+    delegation_city_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    password_hash VARCHAR(64),
+    priority INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(city_id) REFERENCES cities(id),
+    FOREIGN KEY(delegation_city_id) REFERENCES cities(id)
+);
+
+create table reservations(
+    id INT,
+    room_id INT,
+    employee_id VARCHAR(10),
+    reoccurring BOOLEAN,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    create_time TIMESTAMP,
+    last_update_by VARCHAR(10),
+    last_update_time TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY(room_id) REFERENCES rooms(id),
+    FOREIGN KEY(employee_id) REFERENCES employees(id)
+);
