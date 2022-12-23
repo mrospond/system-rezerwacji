408cb00b3510421ebf2c909b989c7ab878fd83de
rfmajor <rfmajor99@gmail.com>
2022-12-23 15:31:08 +0100

    Update

--- src/main/java/com/example/reservationsystem/ReservationSystemApplication.java
+++ src/main/java/com/example/reservationsystem/ReservationSystemApplication.java
@@ -1,7 +1,5 @@
 package com.example.reservationsystem;
 
-import com.example.reservationsystem.database.dao.EmployeeDao;
-import com.example.reservationsystem.reflection.ReflectionUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReservationSystemApplication {
     public static void main(String[] args) {
-        var ctx = SpringApplication.run(ReservationSystemApplication.class, args);
-        var emp = ctx.getBean(EmployeeDao.class).getByEmail("brenda.barry80325@example.com");
-        var gowno = ReflectionUtil.extractFieldsAsValuesArray(emp);
-        System.out.println();
+        SpringApplication.run(ReservationSystemApplication.class, args);
     }
 }
