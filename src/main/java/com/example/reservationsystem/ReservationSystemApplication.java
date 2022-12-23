7407c233f6c3c69fa6a918da0ed753aa6d0d7dc7
rfmajor <rfmajor99@gmail.com>
2022-12-23 15:30:36 +0100

    Implement reflection util for reading class fields

--- src/main/java/com/example/reservationsystem/ReservationSystemApplication.java
+++ src/main/java/com/example/reservationsystem/ReservationSystemApplication.java
@@ -1,5 +1,7 @@
 package com.example.reservationsystem;
 
+import com.example.reservationsystem.database.dao.EmployeeDao;
+import com.example.reservationsystem.reflection.ReflectionUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReservationSystemApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ReservationSystemApplication.class, args);
+        var ctx = SpringApplication.run(ReservationSystemApplication.class, args);
+        var emp = ctx.getBean(EmployeeDao.class).getByEmail("brenda.barry80325@example.com");
+        var gowno = ReflectionUtil.extractFieldsAsValuesArray(emp);
+        System.out.println();
     }
 }
