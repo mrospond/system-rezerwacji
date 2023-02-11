50adec2635ab8ac985c04dfd378738dd8db3d7bd
rfmajor <rfmajor99@gmail.com>
2023-02-11 18:06:13 +0100

    Enable csrf (still not working though)

--- src/main/java/com/example/reservationsystem/config/SecurityConfig.java
+++ src/main/java/com/example/reservationsystem/config/SecurityConfig.java
@@ -13,7 +13,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .cors().disable()
                 .formLogin()
                     .loginProcessingUrl("/login")
